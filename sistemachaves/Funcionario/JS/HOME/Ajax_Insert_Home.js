a60dddaaa20f9322cbe91411890224c189fd1af4
Elton <marcosifba01@gmail.com>
2022-11-02 10:18:15 -0300

    'teste'

--- sistemachaves/Funcionario/JS/HOME/Ajax_Insert_Home.js
+++ sistemachaves/Funcionario/JS/HOME/Ajax_Insert_Home.js
@@ -86,6 +86,7 @@ function GerenciamentoPredios(){
     }
     RefreshChaves();
 
+
     function getChaves(){
         $('.Bloco_Chaves_LI_DISP').remove();
         $('.Bloco_Chaves_LI_USO').remove();
@@ -147,63 +148,14 @@ function GerenciamentoPredios(){
         })
     }
     
-    function Agendar(){
-        $('.Bloco_Chaves_LI_DISP').remove();
-        $('.Bloco_Chaves_LI_USO').remove();
-        //Adicionar Chaves aos Blocos Prédio
-        $.getJSON('./PHP/Gerenciamento/chave.php', function(chaves){
-            $('.Bloco_Chaves_UL').each((index, elementUL) => {
-                $.each(chaves, function (indexC, chave) {
-                    if (("Predio_"+chave.idPredio) == elementUL.id && chave.situacao == 0){
-                            $('.idChave_'+chave.idChave).click(()=>{
-                                    /*----------------------------------------------------------------------------------*/
-                                    var POPUPS = document.querySelector('.POPUPS');
-                                    var div_PopupCadPredios = document.querySelector('.PoPuCadastroPredio'); // Cadastro
-                                    var div_PopupExcluirPredios = document.querySelector('.PoPuExcluirPredio') // Delete
-                                    var div_PopupAgendarChave = document.querySelector('.PoPuAgendarChave') // Agendar
-                                    /*----------------------------------------------------------------------------------*/
-                                    var buttonAgendarChave = document.getElementById('AgendarChave');
-                                    var buttonFechaSubmit = document.getElementById('FechaAgendarChave');
-                                    var buttonSubmitAgendarChave = document.getElementById('SubmitAgendarChave');
-                                    /*----------------------------------------------------------------------------------*/
-                                    document.getElementById('NumeroSalaAgendamento').innerHTML = `${chave.descricao} ${chave.idChave}`;
-                                    document.getElementById('NumeroPredioAgendamento').innerHTML = `Predio ${chave.idPredio}`;
-                                    /*----------------------------------------------------------------------------------*/
-                                        /*Blur*/
-                                        POPUPS.style.display = 'flex';
-                                        POPUPS.classList.add("active");
-                                        /*O que será aberto*/
-                                        div_PopupAgendarChave.style.display = "flex";
-                                        div_PopupAgendarChave.classList.add("active");
-                                        /*O que será oculto*/
-                                        div_PopupCadPredios.style.display = "none";
-                                        div_PopupExcluirPredios.style.display = "none";
-                            
-                                    
-                                    buttonSubmitAgendarChave.addEventListener('click', (event)=>{ // Botão Salvar
-                                        event.preventDefault();
-                                        var aux1 = document.querySelector('#turno_agendamento').value
-                                        var aux2 = document.querySelector('#data_agendamento').value
-                                        alert('as');
-                                    
-                                    buttonFechaSubmit.addEventListener('click', ()=>{ // Botão Fecha
-                                        /*Blur*/
-                                        POPUPS.style.display = 'none';
-                                        POPUPS.classList.remove("active");
-                                        /*O que será aberto*/
-                                        div_PopupAgendarChave.style.display = "none";
-                                        div_PopupAgendarChave.style.display = "none";
-                                    });
-            
-                            })
-                        }
-                    })
-            })
-        })
-    }Agendar();
-    /*---------*/
 }
+/*
+function agendar(){
+    $('.Bloco_Chaves_LI_DISP').each(function(index){
+        console.log(index)
+    })
+}
+agendar();*/
 
 GerenciamentoPredios();
-
-// $(document).load(getChaves());
+/*$(document).load(getChaves());*/
