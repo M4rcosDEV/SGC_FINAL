83cf5509ef508aed1a4a90aa718223c2c5f8e75a
M4rcosDEV <marcosifba01@gmail.com>
2022-11-02 17:19:19 -0300

    SEM SALA NO BANCO

--- sistemachaves/SQL/banco.sql
+++ sistemachaves/SQL/banco.sql
@@ -1,73 +1,3 @@
-<<<<<<< HEAD:sistemachaves/SQL/SGC_BANCO_MOD-ELTON.sql
-/*drop schema sgc;*/
-create schema SGC;
-use SGC;
-
-create table cliente (
-	id_cliente integer auto_increment primary key,
-	nome varchar(50) not null,
-	matricula varchar(15) not null unique,
-	senha varchar(255) not null,
-    tipo_func varchar(55) not null, 
-    email varchar(55) not null unique
-);
-
-create table administrador (
-	id_administrador integer auto_increment primary key,
-	id_funcionario integer,
-	nome varchar(200) not null,
-	matricula varchar(15) not null unique,
-	senha varchar(255) not null,
-	tipo_func varchar(55) not null, 
-    email varchar(55) not null unique
-);
-
-create table predio ( 
-	idPredio integer primary key
-); 
-
-/*
-SITUAÇÕES CHAVE:
-0 - DISPONIVEL
-1 - EM USO
-2 - MANUTENÇÃO
-*/
-create table chave ( 
-	idChave integer primary key,  
-	situacao integer not null,  
-	idPredio integer not null,
-    descricao varchar(50) not null,
-    foreign key(idPredio) references predio(idPredio)
-);
-
-/*Sala vai ser criada de acordo a Chave que vai ser criada,
-ou seja, cria a chave e automáticamente a sala é criada*/
-
-create table sala ( 
-	idSala integer,
-	foreign key (idSala) references chave(idChave),  
-	idPredio integer not null,
-	foreign key (idPredio) references predio(idPredio),
-	primary key(idSala, idPredio)
-); 
-create table agendar (
-	idChave integer,
-    id_cliente integer, 
-	turno integer not null, 
-    data_agendar date not null,  
-	foreign key(idChave) references chave(idChave),
-    foreign key(id_cliente) references cliente(matricula),
-    primary key(idChave, id_cliente)
-);
-
-create table emprestimo (
-	id_emprestimo integer auto_increment primary key,
-    idChave integer,
-    id_cliente integer,
-    id_funcionario integer,
-	foreign key(idChave, id_cliente) references agendar(idChave, id_cliente),
-    foreign key(id_funcionario) references administrador(id_administrador)
-=======
 /*drop schema sgc;*/
 create schema SGC;
 use SGC;
@@ -126,5 +56,4 @@ create table emprestimo (
     id_funcionario integer,
 	foreign key(idChave, id_cliente) references agendar(idChave, id_cliente),
     foreign key(id_funcionario) references administrador(id_administrador)
->>>>>>> 5b8cfdc4584cce1bfed02676d04b6f98ce1e6479:sistemachaves/SQL/banco.sql
 );
